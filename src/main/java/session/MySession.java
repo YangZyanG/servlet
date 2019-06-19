0668406b2f2231702b17d60a30f03a7a5c64745a
yangzy <183243005@qq.com>
2019-06-19 21:33:11 +0800

    yangzy

---
+++ src/main/java/session/MySession.java
@@ -0,0 +1,13 @@
+package session;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class MySession extends HttpServlet {
+
+    @Override
+    public void doGet(HttpServletRequest request, HttpServletResponse response){
+
+    }
+}
